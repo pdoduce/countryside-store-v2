9e978181d088a104442ee94f51df54ebb78e0625
Odukoke Ayoola <pdoduce@gmail.com>
2025-07-02 14:32:44 -0700

    Fix: Add missing supabase server client

---
+++ src/lib/supabase/server.ts
@@ -0,0 +1,7 @@
+
+import { cookies } from 'next/headers'
+import { type Database } from '@/types/supabase' // Optional: if you’ve generated types from Supabase
+
+export const createSupabaseServerClient = () => {
+  
+}
